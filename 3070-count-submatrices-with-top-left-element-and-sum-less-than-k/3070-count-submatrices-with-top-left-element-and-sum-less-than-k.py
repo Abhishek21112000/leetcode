d528389fdb4277ce83d8e73a450398ae76dfb864
Abhishek kaniganti <67180953+Abhishek21112000@users.noreply.github.com>
2026-03-18 01:02:51 -0400

    Time: 311 ms (64.83%), Space: 66.8 MB (35.86%) - LeetHub

---
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+        m, n = len(grid), len(grid[0])
+        s = [[0] * (n+1) for _ in range(m+1)]
+        ans = 0
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                s[i][j] = s[i-1][j] + s[i][j-1] - s[i-1][j-1] + grid[i-1][j-1]
+                if s[i][j] <= k:
+                    ans += 1
+        return ans
+        
