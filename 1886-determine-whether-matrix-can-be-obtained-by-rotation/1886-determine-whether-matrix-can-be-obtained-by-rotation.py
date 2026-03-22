34a40fc825c9374d9e2df5b88d7324ec4a1c9c31
Abhishek kaniganti <67180953+Abhishek21112000@users.noreply.github.com>
2026-03-21 20:41:53 -0400

    Time: 0 ms (100%), Space: 19.3 MB (59.42%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        def rotate90(matrix):     
+            n = len(matrix)
+            return [[matrix[n - 1 - j][i] for j in range(n)] for i in range(n)]
+        for _ in range(4):
+            if mat == target:
+                return True
+            mat = rotate90(mat)
+        return False
