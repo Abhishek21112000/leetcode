3cb9ed9585597294c4dda9e0f8f2f5e0b10596cb
Abhishek kaniganti <67180953+Abhishek21112000@users.noreply.github.com>
2026-03-24 12:45:45 -0400

    Time: 107 ms (76.16%), Space: 42.9 MB (74.17%) - LeetHub

---
+++ 2906-construct-product-matrix/2906-construct-product-matrix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n, m = len(grid), len(grid[0])
+        p = [[0] * m for _ in range(n)]
+        MOD = 12345
+        suf = 1
+        for i in range(n - 1, -1 ,-1):
+            for j in range(m - 1, -1, -1):
+                p[i][j] = suf
+                suf = suf * grid[i][j] % MOD
+        
+        pre = 1
+        for i in range(n):
+            for j in range(m):
+                p[i][j] = p[i][j] * pre % MOD
+                pre = pre * grid[i][j] % MOD
+        return p
