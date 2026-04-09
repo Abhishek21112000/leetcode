c058aae3734fc1221eb816a45632daafb7350701
Abhishek kaniganti <67180953+Abhishek21112000@users.noreply.github.com>
2026-04-08 20:11:48 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,14 +1,30 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        unlocked = {0}
-        changed = True 
-        while changed:
-            changed = False
-            for room in unlocked.copy():
-                for key in rooms[room]:
-                    if key not in unlocked:
-                        unlocked.add(key)
-                        changed = True
-        return len(unlocked) == len(rooms)
+        # unlocked = {0}
+        # changed = True 
+        # while changed:
+        #     changed = False
+        #     for room in unlocked.copy():
+        #         for key in rooms[room]:
+        #             if key not in unlocked:
+        #                 unlocked.add(key)
+        #                 changed = True
+        # return len(unlocked) == len(rooms)
+
+        #DFS
+
+        visit = set()
+        stack = [0]
+
+        while stack:
+            room = stack.pop()
+            if room in visit:
+                continue
+            visit.add(room)
+            for key in rooms[room]:
+                if key not in visit:
+                    stack.append(key)
+        return len(visit) == len(rooms)
+
         
         
